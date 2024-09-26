1a1565f1c07027aa7d988866a100be3fcea11588
Mara Sol <165195529+maraasool10@users.noreply.github.com>
2024-09-26 10:36:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function validarlogin() {
+function verificar() {
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
 
 
-    if((email == " ")  || (senha ==" ")){
-        alert('Preencha os campos')
-    } else{
-        ('login bem-sucedido')
+    if(email == " ") {
+        alert('Preencha os campos!')
+    } else if (senha == " ") {
+        alert('Campo de senha vazio!')
 
-
-
-    }
+   }else{
+    window.location="index(1).html"
+   }
 
 }
